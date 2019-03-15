15edc4af836c4503004380e4ec7d60e4afa6fbcc
Dominik Rowicki <dominik.rowicki@netguru.co>
2019-03-15 23:15:18 +0100

    Save transpiled JS to correct location

--- build.js
+++ build.js
@@ -1,4 +1,8 @@
 const fs = require("fs");
 const src = require("./src/index.js");
 
-fs.writeFileSync("output.json", JSON.stringify(src, null, 2), "utf8");
+fs.writeFileSync(
+  "themes/Dominik's Solarized Dark-color-theme.json",
+  JSON.stringify(src, null, 2),
+  "utf8"
+);
