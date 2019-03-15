a35b0861d2491327d3066e931bc4e589b414040f
Dominik Rowicki <dominik.rowicki@netguru.co>
2019-03-16 00:18:46 +0100

    Use nested objects in colors.js

--- src/colors.js
+++ src/colors.js
@@ -1,53 +1,96 @@
-module.exports = {
-  "widget.shadow": "#000000c0",
-  "selection.background": "#073642",
+const flatten = require("./flatten");
+
+module.exports = flatten({
   focusBorder: "#2aa198",
 
-  "button.background": "#2aa198",
-  "button.foreground": "#fdf6e3",
+  widget: {
+    shadow: "#000000c0"
+  },
 
-  "input.background": "#002b36",
-  "input.foreground": "#839496",
-  "inputOption.activeBorder": "#2aa198",
+  selection: {
+    background: "#073642"
+  },
 
-  "editor.background": "#002b36",
-  "editorCursor.foreground": "#2aa198",
-  "editor.foreground": "#839496",
-  "editorWhitespace.foreground": "#93A1A180",
-  "editor.lineHighlightBackground": "#0A2933",
-  "editor.selectionBackground": "#073642",
-  "editor.selectionForeground": "#93a1a1",
+  button: {
+    background: "#2aa198",
+    foreground: "#fdf6e3"
+  },
 
-  "activityBar.background": "#073642",
-  "activityBar.foreground": "#fdf6e3",
-  "activityBar.inactiveForeground": "#93a1a1",
-  "activityBar.border": "#002b36",
-  "activityBarBadge.background": "#d33682",
+  input: {
+    background: "#002b36",
+    foreground: "#839496"
+  },
 
-  "sideBar.background": "#073642",
-  "sideBar.foreground": "#93a1a1",
-  "sideBar.border": "#002b36",
+  inputOption: {
+    activeBorder: "#2aa198"
+  },
 
-  "editorGroupHeader.tabsBackground": "#073642",
-  "tab.activeBackground": "#002b36",
-  "tab.activeForeground": "#93a1a1",
-  "tab.inactiveBackground": "#073642",
-  "tab.inactiveForeground": "#839496",
-  "tab.hoverBackground": "#002b36",
-  "tab.border": "#073642",
+  editor: {
+    background: "#002b36",
+    foreground: "#839496",
+    lineHighlightBackground: "#0A2933",
+    selectionBackground: "#073642",
+    selectionForeground: "#93a1a1"
+  },
 
-  "notifications.background": "#073642",
-  "notifications.foreground": "#93a1a1",
+  editorCursor: {
+    foreground: "#2aa198"
+  },
 
-  "editorWidget.background": "#073642",
+  editorWhitespace: {
+    foreground: "#93A1A180"
+  },
 
-  "statusBar.background": "#073642",
-  "statusBar.foreground": "#93a1a1",
-  "statusBar.noFolderBackground": "#073642",
-  "statusBar.noFolderForeground": "#93a1a1",
+  activityBar: {
+    background: "#073642",
+    foreground: "#fdf6e3",
+    inactiveForeground: "#93a1a1",
+    border: "#002b36"
+  },
 
-  "titleBar.activeBackground": "#073642",
-  "titleBar.activeForeground": "#93a1a1",
-  "titleBar.inactiveBackground": "#002b36",
-  "titleBar.inactiveForeground": "#839496"
-};
+  activityBarBadge: {
+    background: "#d33682"
+  },
+
+  sidebar: {
+    background: "#073642",
+    foreground: "#93a1a1",
+    border: "#002b36"
+  },
+
+  editorGroupHeader: {
+    tabsBackground: "#073642"
+  },
+
+  tab: {
+    activeBackground: "#002b36",
+    activeForeground: "#93a1a1",
+    inactiveBackground: "#073642",
+    inactiveForeground: "#839496",
+    hoverBackground: "#002b36",
+    border: "#073642"
+  },
+
+  notifications: {
+    background: "#073642",
+    foreground: "#93a1a1"
+  },
+
+  editorWidget: {
+    background: "#073642"
+  },
+
+  statusBar: {
+    background: "#073642",
+    foreground: "#93a1a1",
+    noFolderBackground: "#073642",
+    noFolderForeground: "#93a1a1"
+  },
+
+  titleBar: {
+    activeBackground: "#073642",
+    activeForeground: "#93a1a1",
+    inactiveBackground: "#002b36",
+    inactiveForeground: "#839496"
+  }
+});
