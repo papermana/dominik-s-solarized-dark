892db38d1c947f9939bc492050abf7a6da97e5ab
Dominik Rowicki <dominik.rowicki@netguru.co>
2019-03-16 00:56:47 +0100

    Rename JSON file produced to use kebab-case

--- build.js
+++ build.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const src = require("./src/index.js");
 
 fs.writeFileSync(
-  "themes/Dominik's Solarized Dark-color-theme.json",
+  "themes/dominik-s-solarized-dark-color-theme.json",
   JSON.stringify(src, null, 2),
   "utf8"
 );
